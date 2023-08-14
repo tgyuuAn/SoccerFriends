bb896ee5bee3ca4a327d2e99f71a536286153fa7
tgyuu-An <atg0614@gmail.com>
2023-08-14 18:27:33 +0900

    #4 [ADD] SoccerFriendsApplication 추가

---
+++ presentation/src/main/java/com/tgyuu/soccerfriends/SoccerFriendsApplication.kt
@@ -0,0 +1,8 @@
+package com.tgyuu.soccerfriends
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SoccerFriendsApplication : Application() {
+}
--- presentation/src/main/java/com/tgyuu/soccerfriends/common/base/BaseFragment.kt
+++ presentation/src/main/java/com/tgyuu/soccerfriends/common/base/BaseFragment.kt
@@ -1,18 +1,20 @@
 package com.tgyuu.soccerfriends.common.base
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 
 typealias Inflate<B> = (LayoutInflater, ViewGroup?, Boolean) -> B
 
-class BaseFragment<B : ViewDataBinding>(private val inflate : Inflate<B>) : Fragment() {
+class BaseFragment<B : ViewDataBinding>(private val inflate: Inflate<B>) : Fragment() {
 
-    protected var _binding : B? = null
-    private val binding : B
+    protected var _binding: B? = null
+    private val binding: B
         get() = _binding!!
 
     override fun onCreateView(
@@ -20,9 +22,13 @@ class BaseFragment<B : ViewDataBinding>(private val inflate : Inflate<B>) : Frag
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = inflate.invoke(inflater,container,false)
+        _binding = inflate.invoke(inflater, container, false)
         return binding.root
     }
+
+    fun log(str: String) = Log.d("tgyuu", str)
+
+    fun toast(str: String) = Toast.makeText(requireContext(), str, Toast.LENGTH_SHORT).show()
 
     override fun onDestroyView() {
         super.onDestroyView()

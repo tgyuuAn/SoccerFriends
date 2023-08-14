90e0f0f5ac3b3805a736ad54d9ba87e061c8b7ea
tgyuu-An <atg0614@gmail.com>
2023-08-14 19:14:41 +0900

    #4 [FIX] Splash 에러 수정

--- presentation/src/main/java/com/tgyuu/soccerfriends/MainActivity.kt
+++ presentation/src/main/java/com/tgyuu/soccerfriends/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.tgyuu.soccerfriends
 
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.tgyuu.soccerfriends.common.base.BaseActivity
 import com.tgyuu.soccerfriends.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity<ActivityMainBinding>({ ActivityMainBinding.inflate(it) }) {
-
+    override fun setSplash(){
+        installSplashScreen()
+    }
 }

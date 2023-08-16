2020df732c1d7f4fa16a0ce2e1d9c35dcc935f3b
tgyuu-An <atg0614@gmail.com>
2023-08-16 16:05:02 +0900

    #10 [ADD] Bar클래스 및 xml 추가

---
+++ presentation/src/main/java/com/tgyuu/soccerfriends/common/base/view/Bar.kt
@@ -0,0 +1,10 @@
+package com.tgyuu.soccerfriends.common.base.view
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class Bar(context : Context, attrs : AttributeSet) : ConstraintLayout(context, attrs){
+
+
+}

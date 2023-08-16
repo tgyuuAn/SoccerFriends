2bc9efe78bd797cafe0258f4ee6e6fb4e7d4f0a0
tgyuu-An <atg0614@gmail.com>
2023-08-16 15:32:04 +0900

    #7 [ADD] Home에서 사용할 vector drawable 추가

--- presentation/src/main/java/com/tgyuu/soccerfriends/feature/home/HomeFragment.kt
+++ presentation/src/main/java/com/tgyuu/soccerfriends/feature/home/HomeFragment.kt
@@ -1,12 +1,8 @@
 package com.tgyuu.soccerfriends.feature.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import com.tgyuu.soccerfriends.R
 import com.tgyuu.soccerfriends.common.base.BaseFragment
 import com.tgyuu.soccerfriends.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -14,5 +10,14 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding,HomeViewModel>(FragmentHomeBinding::inflate) {
     override val fragmentViewModel: HomeViewModel by viewModels()
-    
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply{
+            viewModel = fragmentViewModel.apply{
+
+            }
+        }
+    }
 }

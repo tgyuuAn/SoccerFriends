08930bd31efcfe6861a3eac14f96c528830abdac
tgyuu-An <atg0614@gmail.com>
2023-08-16 22:02:39 +0900

    #16 [ADD] repeatOnStarted 확장함수 추가

--- presentation/src/main/java/com/tgyuu/soccerfriends/feature/home/HomeFragment.kt
+++ presentation/src/main/java/com/tgyuu/soccerfriends/feature/home/HomeFragment.kt
@@ -4,20 +4,30 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
 import com.tgyuu.soccerfriends.common.base.BaseFragment
+import com.tgyuu.soccerfriends.common.base.repeatOnStarted
 import com.tgyuu.soccerfriends.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class HomeFragment : BaseFragment<FragmentHomeBinding,HomeViewModel>(FragmentHomeBinding::inflate) {
+class HomeFragment :
+    BaseFragment<FragmentHomeBinding, HomeViewModel>(FragmentHomeBinding::inflate) {
     override val fragmentViewModel: HomeViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.apply{
-            viewModel = fragmentViewModel.apply{
-
+        binding.apply {
+            viewModel = fragmentViewModel.apply {
+                repeatOnStarted {
+                    eventFlow.collect { handleEvent(it) }
+                }
             }
+        }
+    }
+
+    private fun handleEvent(event : HomeViewModel.HomeEvent){
+        when(event){
+            is open
         }
     }
 }

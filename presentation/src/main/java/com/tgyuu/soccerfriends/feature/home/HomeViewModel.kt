ea23d951219d6774f8168347c14db29666884b18
tgyuu-An <atg0614@gmail.com>
2023-08-16 21:58:50 +0900

    #16 [ADD] ViewModel ClickButton 이벤트 추가

--- presentation/src/main/java/com/tgyuu/soccerfriends/feature/home/HomeViewModel.kt
+++ presentation/src/main/java/com/tgyuu/soccerfriends/feature/home/HomeViewModel.kt
@@ -1,10 +1,28 @@
 package com.tgyuu.soccerfriends.feature.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor() : ViewModel() {
 
+    private val _eventFlow = MutableSharedFlow<HomeEvent>()
+    val eventFlow = _eventFlow.asSharedFlow()
+
+    fun event(event : HomeEvent){
+        viewModelScope.launch{
+            _eventFlow.emit(event)
+        }
+    }
+
+    fun clickButton() = event(HomeEvent.ClickButton)
+
+    sealed class HomeEvent(){
+        object ClickButton : HomeEvent()
+    }
 }

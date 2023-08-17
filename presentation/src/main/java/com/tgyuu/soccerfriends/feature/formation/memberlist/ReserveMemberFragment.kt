2e1b4e3ca55ed60ee77b40baab889cd8422d2ea4
tgyuu-An <atg0614@gmail.com>
2023-08-17 21:16:46 +0900

    #13 [ADD] 리사이클러뷰 장착

--- presentation/src/main/java/com/tgyuu/soccerfriends/feature/formation/memberlist/ReserveMemberFragment.kt
+++ presentation/src/main/java/com/tgyuu/soccerfriends/feature/formation/memberlist/ReserveMemberFragment.kt
@@ -6,20 +6,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.tgyuu.soccerfriends.R
 import com.tgyuu.soccerfriends.common.base.BaseFragment
 import com.tgyuu.soccerfriends.databinding.FragmentReserveMemberBinding
 import com.tgyuu.soccerfriends.feature.formation.FormationViewModel
+import com.tgyuu.soccerfriends.feature.formation.memberlist.recyclerview.FormationTeamListAdapter
+import com.tgyuu.soccerfriends.feature.team.recyclerview.TeamListAdapter
 
 class ReserveMemberFragment :
     BaseFragment<FragmentReserveMemberBinding, FormationViewModel>(FragmentReserveMemberBinding::inflate) {
 
     override val fragmentViewModel: FormationViewModel by viewModels(ownerProducer = { requireParentFragment() })
+    private val formationTeamListAdapter: FormationTeamListAdapter by lazy { FormationTeamListAdapter() }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.viewModel=fragmentViewModel.apply{
 
+        }
+        setRecyclerView()
+    }
+
+    private fun setRecyclerView() = binding.apply{
+        reserveMemberListRV.apply{
+            adapter = formationTeamListAdapter
+            layoutManager = LinearLayoutManager(requireActivity())
         }
     }
 }
--- presentation/src/main/java/com/tgyuu/soccerfriends/feature/formation/memberlist/SelectionMemberFragment.kt
+++ presentation/src/main/java/com/tgyuu/soccerfriends/feature/formation/memberlist/SelectionMemberFragment.kt
@@ -6,20 +6,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.tgyuu.soccerfriends.R
 import com.tgyuu.soccerfriends.common.base.BaseFragment
 import com.tgyuu.soccerfriends.databinding.FragmentSelectionMemberBinding
 import com.tgyuu.soccerfriends.feature.formation.FormationViewModel
+import com.tgyuu.soccerfriends.feature.formation.memberlist.recyclerview.FormationTeamListAdapter
 
 class SelectionMemberFragment :
     BaseFragment<FragmentSelectionMemberBinding, FormationViewModel>(FragmentSelectionMemberBinding::inflate) {
 
     override val fragmentViewModel: FormationViewModel by viewModels(ownerProducer = { requireParentFragment() })
+    private val formationTeamListAdapter: FormationTeamListAdapter by lazy { FormationTeamListAdapter() }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.viewModel=fragmentViewModel.apply{
+        }
+        setRecyclerView()
+    }
 
+    private fun setRecyclerView() = binding.apply{
+        selectionMemberListRV.apply{
+            adapter = formationTeamListAdapter
+            layoutManager = LinearLayoutManager(requireActivity())
         }
     }
 }

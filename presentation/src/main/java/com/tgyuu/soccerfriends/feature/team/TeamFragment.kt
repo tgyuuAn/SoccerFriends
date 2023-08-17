08e86b29391a24a00b58282d07cbc4c100e637fa
tgyuu-An <atg0614@gmail.com>
2023-08-17 15:29:45 +0900

    #14 [ADD] 리사이클러뷰 어댑터 장착

--- presentation/src/main/java/com/tgyuu/soccerfriends/feature/team/TeamFragment.kt
+++ presentation/src/main/java/com/tgyuu/soccerfriends/feature/team/TeamFragment.kt
@@ -3,19 +3,33 @@ package com.tgyuu.soccerfriends.feature.team
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.tgyuu.soccerfriends.common.base.BaseFragment
 import com.tgyuu.soccerfriends.databinding.FragmentTeamBinding
+import com.tgyuu.soccerfriends.feature.team.recyclerview.TeamListAdapter
 
 class TeamFragment :
     BaseFragment<FragmentTeamBinding, TeamViewModel>(FragmentTeamBinding::inflate) {
     override val fragmentViewModel: TeamViewModel by viewModels()
+    private val teamListAdapter: TeamListAdapter by lazy { TeamListAdapter() }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.apply {
-            viewModel = fragmentViewModel.apply {
-
-            }
+        binding.viewModel = fragmentViewModel.apply {
         }
+        setRecyclerView()
     }
+
+    private fun setRecyclerView() =
+        binding.teamListRV.apply {
+            adapter = teamListAdapter
+            layoutManager = LinearLayoutManager(requireActivity())
+            addItemDecoration(
+                DividerItemDecoration(
+                    requireContext(),
+                    DividerItemDecoration.VERTICAL
+                )
+            )
+        }
 }

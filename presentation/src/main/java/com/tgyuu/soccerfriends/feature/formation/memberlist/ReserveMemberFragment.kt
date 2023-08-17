568a4cd4eaeee20855f9436dd4bf37eeec65022a
tgyuu-An <atg0614@gmail.com>
2023-08-17 20:55:41 +0900

    #13 [ADD] Selection, Reserve 각각 Formation 부모 뷰모델 연결

--- presentation/src/main/java/com/tgyuu/soccerfriends/feature/formation/memberlist/ReserveMemberFragment.kt
+++ presentation/src/main/java/com/tgyuu/soccerfriends/feature/formation/memberlist/ReserveMemberFragment.kt
@@ -5,17 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.tgyuu.soccerfriends.R
 import com.tgyuu.soccerfriends.databinding.FragmentReserveMemberBinding
+import com.tgyuu.soccerfriends.feature.formation.FormationViewModel
 
 class ReserveMemberFragment : Fragment() {
+
+    private val fragmentViewModel: FormationViewModel by viewModels(ownerProducer = { requireParentFragment() })
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentReserveMemberBinding.inflate(inflater,container,false)
+        val binding = FragmentReserveMemberBinding.inflate(inflater, container, false)
         return binding.root
     }
 }
--- presentation/src/main/java/com/tgyuu/soccerfriends/feature/formation/memberlist/SelectionMemberFragment.kt
+++ presentation/src/main/java/com/tgyuu/soccerfriends/feature/formation/memberlist/SelectionMemberFragment.kt
@@ -5,17 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.tgyuu.soccerfriends.R
 import com.tgyuu.soccerfriends.databinding.FragmentSelectionMemberBinding
+import com.tgyuu.soccerfriends.feature.formation.FormationViewModel
 
 class SelectionMemberFragment : Fragment() {
+
+    private val fragmentViewModel: FormationViewModel by viewModels(ownerProducer = { requireParentFragment() })
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentSelectionMemberBinding.inflate(inflater,container,false)
+        val binding = FragmentSelectionMemberBinding.inflate(inflater, container, false)
         return binding.root
     }
 }

d343318a40aa33c06c8fa242c2739ed7c65c3430
tgyuu-An <atg0614@gmail.com>
2023-08-17 17:25:39 +0900

    #13 [ADD] 선발, 후보선수 Fragment 추가

---
+++ presentation/src/main/java/com/tgyuu/soccerfriends/feature/formation/memberlist/ReserveMemberFragment.kt
@@ -0,0 +1,12 @@
+package com.tgyuu.soccerfriends.feature.formation.memberlist
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.tgyuu.soccerfriends.R
+
+class ReserveMemberFragment : Fragment() {
+
+}

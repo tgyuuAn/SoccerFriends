513a082ede2e10e0e7633f4f30ce44743319cbc5
tgyuu-An <atg0614@gmail.com>
2023-08-18 13:48:15 +0900

    #22 [ADD] 팀 추가 네비게이션 연결

--- presentation/src/main/java/com/tgyuu/soccerfriends/feature/formation/FormationViewModel.kt
+++ presentation/src/main/java/com/tgyuu/soccerfriends/feature/formation/FormationViewModel.kt
@@ -2,9 +2,14 @@ package com.tgyuu.soccerfriends.feature.formation
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class FormationViewModel @Inject constructor() : ViewModel() {
+
 }

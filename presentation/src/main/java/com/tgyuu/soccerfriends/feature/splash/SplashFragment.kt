1c2eef0527790e299e08f4b8c56d188208998c18
tgyuu-An <atg0614@gmail.com>
2023-08-18 13:19:46 +0900

    #20 [FEAT] Splash, Home 연결

--- presentation/src/main/java/com/tgyuu/soccerfriends/feature/splash/SplashFragment.kt
+++ presentation/src/main/java/com/tgyuu/soccerfriends/feature/splash/SplashFragment.kt
@@ -3,13 +3,17 @@ package com.tgyuu.soccerfriends.feature.splash
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.tgyuu.soccerfriends.R
 import com.tgyuu.soccerfriends.common.base.BaseFragment
+import com.tgyuu.soccerfriends.common.base.repeatOnStarted
 import com.tgyuu.soccerfriends.databinding.FragmentSplashBinding
+import com.tgyuu.soccerfriends.feature.scoreboard.ScoreBoardViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SplashFragment : BaseFragment<FragmentSplashBinding,SplashViewModel>(FragmentSplashBinding::inflate) {
+class SplashFragment :
+    BaseFragment<FragmentSplashBinding, SplashViewModel>(FragmentSplashBinding::inflate) {
 
     override val fragmentViewModel: SplashViewModel by viewModels()
 
@@ -18,9 +22,18 @@ class SplashFragment : BaseFragment<FragmentSplashBinding,SplashViewModel>(Fragm
 
         setStatusBarAndIconColor(R.color.main, StatusBarIconColor.WHITE)
 
-        binding.apply{
-            viewModel = fragmentViewModel.apply{
+        binding.apply {
+            viewModel = fragmentViewModel.apply {
+                repeatOnStarted {
+                    eventFlow.collect { handleEvent(it) }
+                }
+            }
+        }
+    }
 
+    private fun handleEvent(event: SplashViewModel.SplashEvent) {
+        when (event) {
+            SplashViewModel.SplashEvent.Splash -> findNavController().navigate(R.id.action_global_home_nav)
             }
         }
     }

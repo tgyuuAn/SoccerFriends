d1f6fb86a493b2a384512797f6dba01f807c17c4
tgyuu-An <atg0614@gmail.com>
2023-08-18 23:01:02 +0900

    #24 [ADD] UnitTest Coroutine Rule 추가

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
     implementation("com.google.dagger:hilt-android:2.44")
+    testImplementation("junit:junit:4.12")
     kapt("com.google.dagger:hilt-android-compiler:2.44")
 
     implementation("com.github.bumptech.glide:glide:4.15.1")
---
+++ presentation/src/test/java/com/tgyuu/soccerfriends/feature/team/addmember/AddMemberViewModelTest.kt
@@ -0,0 +1,22 @@
+package com.tgyuu.soccerfriends.feature.team.addmember
+
+import org.junit.After
+import org.junit.Assert.*
+import org.junit.Before
+
+
+class AddMemberViewModelTest{
+
+    
+
+    @Before
+    fun setUp(){
+
+    }
+
+    @After
+    fun tearDown(){
+
+    }
+
+}

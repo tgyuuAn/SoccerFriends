f49429a44a606cb07dba24cbe6775bd4feeea685
tgyuu-An <atg0614@gmail.com>
2023-08-20 17:06:30 +0900

    #24 [FEAT] ChangeDialog 추가

---
+++ presentation/src/main/java/com/tgyuu/presentation/feature/team/dialog/ChangeDialogFragment.kt
@@ -0,0 +1,57 @@
+package com.tgyuu.presentation.feature.team.dialog
+
+import android.app.Dialog
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.viewModels
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.tgyuu.presentation.R
+import com.tgyuu.presentation.databinding.FragmentChangeDialogBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ChangeDialogFragment : DialogFragment() {
+
+    companion object {
+        const val TAG = "ChangeDialogFragment"
+    }
+
+    private val fragmentViewModel: ChangeDialogViewModel by viewModels()
+    private var _binding: FragmentChangeDialogBinding? = null
+    val binding get() = _binding!!
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return MaterialAlertDialogBuilder(requireActivity()).apply {
+            _binding = FragmentChangeDialogBinding.inflate(layoutInflater, null, false)
+            setView(onCreateView(layoutInflater, binding.rootLayout, savedInstanceState))
+        }.create()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View = binding.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setBinding()
+    }
+
+    private fun setBinding() = binding.apply {
+        viewModel = fragmentViewModel.apply {
+            viewLifecycleOwner.apply {
+
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}

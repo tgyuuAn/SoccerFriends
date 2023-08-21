cc9c10eb614343da9912a3ed02f9aaa344616507
tgyuu-An <atg0614@gmail.com>
2023-08-22 00:24:13 +0900

    #24 [FEAT] UpdateMemberInformationUseCase 로 이름 변경

--- domain/src/main/java/com/tgyuu/domain/usecase/UpdateMemberInformationUseCase.kt
+++ domain/src/main/java/com/tgyuu/domain/usecase/UpdateMemberInformationUseCase.kt
@@ -3,6 +3,6 @@ package com.tgyuu.domain.usecase
 import com.tgyuu.domain.repository.MemberRepository
 import javax.inject.Inject
 
-class ChangeMemberInformationUseCase @Inject constructor(private val memberReposiory: MemberRepository) {
-    
+class UpdateMemberInformationUseCase @Inject constructor(private val memberReposiory: MemberRepository) {
+
 }

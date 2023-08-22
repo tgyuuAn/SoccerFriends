be3fbd18cd4d514b20f92dab0da989c212f81d0d
tgyuu-An <atg0614@gmail.com>
2023-08-22 14:25:54 +0900

    #25 [FEAT] 기능 구현 전 테스트 코드 작성

---
+++ presentation/src/test/java/com/tgyuu/presentation/feature/scoreboard/ScoreBoardViewModelTest.kt
@@ -0,0 +1,117 @@
+package com.tgyuu.presentation.feature.scoreboard
+
+import com.tgyuu.presentation.rule.MainCoroutineRule
+import org.junit.After
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class ScoreBoardViewModelTest{
+
+    @get:Rule
+    val mainCoroutineRule = MainCoroutineRule()
+
+    lateinit var scoreBoardViewModel: ScoreBoardViewModel
+
+    @Before
+    fun setUp(){
+        scoreBoardViewModel = ScoreBoardViewModel()
+    }
+
+    @Test
+    fun `플레이 타임의 기본 값은 0이다`(){
+        //given
+
+
+        //when
+
+
+        //then
+
+    }
+
+    @Test
+    fun `플레이 타임의 +버튼을 누르면 시간이 1분 증가한다`(){
+        //given
+
+
+        //when
+
+
+        //then
+
+    }
+
+    @Test
+    fun `플레이 타임의 -버튼을 누르면 시간이 1분 감소한다`(){
+        //given
+
+
+        //when
+
+
+        //then
+
+    }
+
+    @Test
+    fun `플레이 타임 설정이 0분일 때 -를 눌러도 감소하지 않는다`(){
+        //given
+
+
+        //when
+
+
+        //then
+
+    }
+
+    @Test
+    fun `경기 종료 전 알림의 기본 값은 0이다`(){
+        //given
+
+
+        //when
+
+
+        //then
+
+    }
+
+    @Test
+    fun `경기 종료 전 알림의 +버튼을 누르면 시간이 1분 증가한다`(){
+        //given
+
+
+        //when
+
+
+        //then
+
+    }
+
+    @Test
+    fun `경기 종료 전 알림의 -버튼을 누르면 시간이 1분 감소한다`(){
+        //given
+
+
+        //when
+
+
+        //then
+
+    }
+
+    @Test
+    fun `경기 종료 전 알림이 0분일 때 -를 눌러도 감소하지 않는다`(){
+        //given
+
+
+        //when
+
+
+        //then
+
+    }
+}

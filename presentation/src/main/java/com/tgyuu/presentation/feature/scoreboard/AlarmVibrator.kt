eff24676752910d77c572f5e6f165dd1cad42abb
tgyuu-An <atg0614@gmail.com>
2023-08-26 19:41:29 +0900

    #25 [CHORE] Lottie View 사이즈 조절

--- presentation/src/main/java/com/tgyuu/presentation/feature/scoreboard/AlarmVibrator.kt
+++ presentation/src/main/java/com/tgyuu/presentation/feature/scoreboard/AlarmVibrator.kt
@@ -15,11 +15,11 @@ class AlarmVibrator @Inject constructor(@ApplicationContext context: Context) {
     private val vibrator: Vibrator by lazy {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             val vibratorManager =
-                context?.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+                context.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
             vibratorManager.defaultVibrator
         } else {
             @Suppress("DEPRECATION")
-            context?.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+            context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
         }
     }
 

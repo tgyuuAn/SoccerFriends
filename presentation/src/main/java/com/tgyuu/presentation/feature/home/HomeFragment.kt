423c3d8c7550e365c4f1c7478a01953ee2e79b47
tgyuu-An <atg0614@gmail.com>
2023-08-28 15:55:21 +0900

    #30 [FEAT] 어플 실행 시 갤러리 접근 권한 받는 로직 추가

--- presentation/src/main/java/com/tgyuu/presentation/feature/home/HomeFragment.kt
+++ presentation/src/main/java/com/tgyuu/presentation/feature/home/HomeFragment.kt
@@ -1,7 +1,10 @@
 package com.tgyuu.presentation.feature.home
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavOptions
 import com.tgyuu.presentation.R
@@ -9,6 +12,7 @@ import com.tgyuu.presentation.common.base.BaseFragment
 import com.tgyuu.presentation.common.base.repeatOnStarted
 import com.tgyuu.presentation.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.security.Permission
 
 @AndroidEntryPoint
 class HomeFragment :
@@ -25,16 +29,49 @@ class HomeFragment :
                 }
             }
         }
-        
+
+        getReadMediaPermission()
     }
+
+    fun getReadMediaPermission(){
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            checkPermission(Manifest.permission.READ_MEDIA_IMAGES)
+        } else {
+            checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+    }
+
+    private fun checkPermission(permission: String) {
+        galleryPermissionLauncher.launch(permission)
+    }
+
+    private val galleryPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+
+            } else {
+                toast("갤러리 접근 권한이 반드시 있어야 합니다.")
+                requireActivity().finish()
+            }
+        }
 
     private fun handleEvent(event: HomeViewModel.HomeEvent) {
         when (event) {
             HomeViewModel.HomeEvent.MoveToScoreBoard -> navigateWithUriNavOptions(
-                "soccerfriends://score_board_nav",NavOptions.Builder().setPopUpTo(R.id.homeFragment,false).build()
+                "soccerfriends://score_board_nav",
+                NavOptions.Builder().setPopUpTo(R.id.homeFragment, false).build()
             )
-            HomeViewModel.HomeEvent.MoveToTeamManagement -> navigateWithUriNavOptions("soccerfriends://team_nav",NavOptions.Builder().setPopUpTo(R.id.homeFragment,false).build())
-            HomeViewModel.HomeEvent.MoveToFormation -> navigateWithUriNavOptions("soccerfriends://formation_nav",NavOptions.Builder().setPopUpTo(R.id.homeFragment,false).build())
+
+            HomeViewModel.HomeEvent.MoveToTeamManagement -> navigateWithUriNavOptions(
+                "soccerfriends://team_nav",
+                NavOptions.Builder().setPopUpTo(R.id.homeFragment, false).build()
+            )
+
+            HomeViewModel.HomeEvent.MoveToFormation -> navigateWithUriNavOptions(
+                "soccerfriends://formation_nav",
+                NavOptions.Builder().setPopUpTo(R.id.homeFragment, false).build()
+            )
         }
     }
 }

4e36482f02d26c08f208024bbce3bc06de97a868
tgyuu-An <atg0614@gmail.com>
2023-08-30 01:24:20 +0900

    #30 [CORRECT] GlideApp -> Glide로 변경

--- presentation/src/main/java/com/tgyuu/presentation/feature/team/TeamFragment.kt
+++ presentation/src/main/java/com/tgyuu/presentation/feature/team/TeamFragment.kt
@@ -15,7 +15,6 @@ import com.bumptech.glide.Glide
 import com.tgyuu.domain.entity.Member
 import com.tgyuu.domain.entity.Team
 import com.tgyuu.presentation.R
-import com.tgyuu.presentation.common.GlideApp
 import com.tgyuu.presentation.common.base.BaseFragment
 import com.tgyuu.presentation.common.base.UiState
 import com.tgyuu.presentation.common.base.repeatOnStarted
@@ -273,7 +272,7 @@ class TeamFragment :
             return
         }
 
-        GlideApp.with(requireContext())
+        Glide.with(requireContext())
             .load(team.image.toUri())
             .circleCrop()
             .into(binding.teamLogoIV)
